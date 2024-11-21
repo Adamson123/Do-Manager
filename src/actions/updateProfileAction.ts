4818f509e2d9c22c26da784828c1fdfe48f83fd6
Ajibade Adam <111712386+Adamson123@users.noreply.github.com>
2024-11-21 17:55:00 +0100

    Update updateProfileAction.ts to make vercel blob overwrite existing user image

--- src/actions/updateProfileAction.ts
+++ src/actions/updateProfileAction.ts
@@ -7,7 +7,7 @@ import { put } from "@vercel/blob";
 import * as z from "zod";
 
 const updateProfileAction = async (formData: FormData) => {
-  const profile = Object.fromEntries(formData.entries()) as z.infer<typeof profileSchema>;
+  const profile = Object.fromEntries(formData.entries()) as z.infer<typeof profileSchema> & { imageId: string };
 
   const validation = profileSchema.safeParse(profile);
   if (!validation.success) {
@@ -15,38 +15,53 @@ const updateProfileAction = async (formData: FormData) => {
     return { errMsg: simplifyError(errors)[0] };
   }
 
-  const { name, userId, image } = validation.data;
-
+  const { name, userId, image, imageId } = validation.data;
+  let user;
   try {
+    let blobUrl;
+
     if (image && typeof image !== "string") {
       const data = await image.arrayBuffer();
-      const blobUrl = await put(
-        `profiles/${userId}.webp`,
-        Buffer.from(data),
-        {
+      const fileName = `profiles/${userId}.webp`;
+
+      // Extract the userId from the imageId URL
+      const imageFileName = new URL(imageId).pathname.split("/").pop();
+      const imageUserId = imageFileName?.split("-")[0]; // Extract userId from the filename
+
+      if (imageUserId === userId) {
+        // Overwrite the existing image
+        const result = await put(fileName, Buffer.from(data), {
           access: "public",
           contentType: "image/webp",
           token: process.env.BLOB_READ_WRITE_TOKEN,
-        }
-      );
+          overwrite: true,
+        });
+        blobUrl = result.url;
+      } else {
+        // Create a new image
+        const result = await put(fileName, Buffer.from(data), {
+          access: "public",
+          contentType: "image/webp",
+          token: process.env.BLOB_READ_WRITE_TOKEN,
+        });
+        blobUrl = result.url;
+      }
 
-      const user = await prisma.user.update({
+      user = await prisma.user.update({
         where: { id: userId },
         data: {
           name,
-          image: blobUrl.url,
+          image: blobUrl,
         },
       });
-
-      return user;
     } else {
-      const user = await prisma.user.update({
+      // If no new image is provided, just update the name
+      user = await prisma.user.update({
         where: { id: userId },
         data: { name },
       });
-
-      return user;
     }
+    return user;
   } catch (err) {
     console.error("Error updating profile:", (err as Error).message);
     return { errMsg: "Error updating profile" };
