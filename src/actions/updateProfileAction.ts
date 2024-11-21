734be3fe1f6ce67902810d57152a9b5103475359
Ajibade Adam <111712386+Adamson123@users.noreply.github.com>
2024-11-21 17:20:52 +0100

    Update updateProfileAction.ts

--- src/actions/updateProfileAction.ts
+++ src/actions/updateProfileAction.ts
@@ -1,20 +1,15 @@
 "use server";
 
-import fsSync from "fs";
 import prisma from "../../prisma/client";
 import { profileSchema } from "@/schemas";
 import simplifyError from "@/utils/simplifyError";
-import { v4 as uuidv4 } from "uuid";
-import * as z from "zod";
 import { put } from "@vercel/blob";
+import * as z from "zod";
 
 const updateProfileAction = async (formData: FormData) => {
-  const profile = Object.fromEntries(formData.entries()) as z.infer<
-    typeof profileSchema
-  > & { imageId: string };
+  const profile = Object.fromEntries(formData.entries()) as z.infer<typeof profileSchema>;
 
   const validation = profileSchema.safeParse(profile);
-  //check if validation is successful
   if (!validation.success) {
     const errors = validation.error.format();
     return { errMsg: simplifyError(errors)[0] };
@@ -22,49 +17,38 @@ const updateProfileAction = async (formData: FormData) => {
 
   const { name, userId, image } = validation.data;
 
-  let user;
   try {
-    if (image) {
-      //// const imageId = uuidv4();
-      // Upload new image to Vercel Blob
-      if (typeof image !== "string") {
-        const data = await image.arrayBuffer();
-        const blobUrl = await put(
-          `profiles/${userId}.webp`,
-          Buffer.from(data),
-          {
-            access: "public",
-            contentType: "image/webp",
-            token: process.env.BLOB_READ_WRITE_TOKEN,
-          },
-        );
+    if (image && typeof image !== "string") {
+      const data = await image.arrayBuffer();
+      const blobUrl = await put(
+        `profiles/${userId}.webp`,
+        Buffer.from(data),
+        {
+          access: "public",
+          contentType: "image/webp",
+          token: process.env.BLOB_READ_WRITE_TOKEN,
+        }
+      );
 
-        user = await prisma.user.update({
-          where: {
-            id: userId,
-          },
-          data: {
-            name,
-            image: blobUrl.url,
-          },
-        });
-        console.log("Image uploaded to Vercel Blob:", blobUrl);
-      }
-    } else {
-      user = await prisma.user.update({
-        where: {
-          id: userId,
-        },
+      const user = await prisma.user.update({
+        where: { id: userId },
         data: {
           name,
+          image: blobUrl.url,
         },
       });
-    }
 
-    return user;
+      return user;
+    } else {
+      const user = await prisma.user.update({
+        where: { id: userId },
+        data: { name },
+      });
+
+      return user;
+    }
   } catch (err) {
-    const error = err as Error;
-    console.log(error.message, "error patch profile -action /user");
+    console.error("Error updating profile:", (err as Error).message);
     return { errMsg: "Error updating profile" };
   }
 };
