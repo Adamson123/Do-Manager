b4e27cea6b3cc55bc3d027e87908c556a5e633eb
Ajibade Adam <111712386+Adamson123@users.noreply.github.com>
2024-11-25 14:12:38 +0100

    Update Tasks.tsx

--- src/components/Home/Tasks/Tasks.tsx
+++ src/components/Home/Tasks/Tasks.tsx
@@ -81,7 +81,7 @@ const Tasks = ({ setOpenDrawer }: TasksProps) => {
       (task) => task.priority !== priority
     );
     const result = [...tasksSortedByPriority, ...otherTasks].filter((task) =>
-      task.title.toLowerCase().trim(" ").includes(search.toLowerCase())
+      task.title.toLowerCase().includes(search.toLowerCase().trim(" "))
     );
     return result;
   }, [tasks, priority, search]);
